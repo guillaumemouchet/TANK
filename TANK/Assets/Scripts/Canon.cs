c8899b9462ec14d594caf9e60427ad45eab02f4c
Dorian Tan <doriantan7@gmail.com>
2022-08-25 12:51:04 +0200

    add test Tank

---
+++ TANK/Assets/Scripts/Canon.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canon : MonoBehaviour
+{
+
+    public GameObject missile;
+    public float launchForce;
+    public Transform firePoint;
+
+    public GameObject point;
+    GameObject[] points;
+    public int numberOfPoints;
+    public float spaceBetweenPoints;
+    Vector2 direction;
+
+    private void Start()
+    {
+        points = new GameObject[numberOfPoints];
+        for (int i = 0; i < numberOfPoints; i++)
+        {
+            points[i] = Instantiate(point, firePoint.position, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 canonPos = transform.position;
+        Vector2 mousePOs = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = mousePOs - canonPos;
+        transform.right = direction;
+
+        if(Input.GetMouseButtonDown(1)) // mouse btn 0 = clicque droit
+        {
+            Shoot();
+        }
+
+        for (int i = 0; i < numberOfPoints; i++)
+        {
+            points[i].transform.position = PointPosition(i * spaceBetweenPoints);
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject newMissile = Instantiate(missile, firePoint.position, firePoint.rotation);
+        newMissile.GetComponent<Rigidbody2D>().velocity = transform.right * launchForce;
+    }
+
+    Vector2 PointPosition(float t)
+    {
+        Vector2 position = (Vector2)firePoint.position + (direction.normalized * launchForce * t ) + 0.5f * Physics2D.gravity * (t * t); // rien compris
+        return position;
+    }
+}
