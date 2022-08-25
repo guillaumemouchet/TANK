6cfa6e16bd51cf316fb2743c208f1d8dc452cd7d
Dorian Tan <doriantan7@gmail.com>
2022-08-25 16:52:38 +0200

    try multiple actions with btn

---
+++ TANK/Assets/Scripts/ActionManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionManager : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Jump()
+    {
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 7f);
+        }
+    }
+}
