a63c5d2e8495a462bfab18903b2e477a49e1bf77
Guillaume <guillaume.mouchet@he-arc.ch>
2022-08-26 11:11:59 +0200

    Correction des Container Displayer

---
+++ TANK/Assets/Scripts/PlayerButton.cs
@@ -0,0 +1,33 @@
+/*
+ * Title : Player Button 
+ * Authors : Titus Abele, Benjamin Mouchet, Guillaume Mouchet, Dorian Tan
+ * Date : 26.08.2022
+ * Source : https://www.youtube.com/watch?v=onDorc3Qfn0 
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerButton : MonoBehaviour
+{
+    [SerializeField] private TMP_Text playerName;
+
+    public void DisplayPlayer()
+    {
+        Debug.Log("playername " + playerName.text);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
