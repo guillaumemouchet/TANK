171b910d3ac3145ab365162fc3da5cbc81abb551
dorian.tan <dorian.tan@he-arc.ch>
2022-09-03 11:27:17 +0200

    Sprites add

--- TANK/Assets/Scripts/TankController.cs
+++ TANK/Assets/Scripts/TankController.cs
@@ -43,6 +43,7 @@ public class TankController : MonoBehaviour
 
             Destroy(collision.gameObject);
         }
+
     }
 
     /***************************************************************\
